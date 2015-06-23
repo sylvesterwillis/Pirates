a7f0f92898d67e3111efb36b2edc2a5592e841a8
Sylvester Willis <swillis16@student.gsu.edu>
2015-06-22 20:53:45 -0400

    Initial commit of pirate code for SCA.

---
+++ Pirates/Controllers/PiratesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Pirates.Controllers {
+    public class PiratesController : ApiController {
+        const int ALLOWED_PIRATES = 1000;
+        public IHttpActionResult GetGold(int pirateCount) {
+            long result = 1;
+
+            if (pirateCount >= ALLOWED_PIRATES) {
+                return Ok("Number of pirates must be lower than 1000.");
+            }
+            if (pirateCount <= 0)
+            {
+                return Ok("Number of pirates must be greater than 0.");
+            }
+
+            for (int i = 0; i <= pirateCount; i++) {
+                result = (result * pirateCount) + 1;
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Pirates/static/script.js
@@ -0,0 +1,27 @@
+﻿var app = angular.module('main', []).
+controller('DemoCtrl', function ($scope, $http) {
+	$scope.getGold = function () {
+		var pirateCount = $('#pirateCount').val();
+		var resultDiv = $('#result');
+
+		if (!$.isNumeric(pirateCount)) {
+			resultDiv.html('Please enter a number.');
+		}
+		else {
+			var url = '/api/pirates/' + pirateCount;
+			console.log(url);
+			resultDiv.html('Waiting for result...');
+
+			$http({
+				method: 'GET',
+				url: url
+			})
+			.success(function (data) {
+				resultDiv.html(data);
+			})
+			.error(function (data, status) {
+				resultDiv.html(data);
+			});
+		}
+	};
+});
